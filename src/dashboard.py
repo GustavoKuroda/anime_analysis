4c2c15b66604feaedcd1365d2713e4c8ac02e8bc
Kuroda <guga.kenji1214@gmail.com>
2023-11-21 11:48:39 -0300

    Change to Streamlit

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,25 +1,30 @@
-from dash import Dash, html, dcc, callback, Output, Input
+import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv(f'D:\\Documentos\\Portifolio\\anime_analysis\\src\\data\\imdb_anime_enhanced.csv')
+# Carregando a base de dados (substitua 'seu_arquivo.csv' pelo nome real do seu arquivo)
+df = pd.read_csv('D:\\Documentos\\Portifolio\\anime_analysis\\src\\data\\imdb_anime_enhanced.csv')
 
-hist = px.histogram(df, 
-                    x='User Rating', 
-                    nbins=int(1 + 10 * (df['User Rating'].max() - df['User Rating'].min())),
-                    color='Init_year',
-                    color_discrete_sequence=px.colors.sequential.Reds,
-                    marginal='histogram',
-                    title='Distribution of User Rating.')
-hist.update_layout(showlegend=False)
-hist.update_traces(showlegend=False)
+# Configuração do título
+st.title('Anime Dashboard')
 
-app = Dash(__name__)
+# Filtros interativos
+ano_filtro = st.slider('Filtrar por Ano:', int(df['Init_year'].min()), int(df['Init_year'].max()), (int(df['Init_year'].min()), int(df['Init_year'].max())))
+genero_filtro = st.multiselect('Filtrar por Gênero:', ['Todos'] + list(df['Genre'].unique()), default=['Todos'])
+ongoing_filtro = st.selectbox('Filtrar por Status de Andamento:', ['Todos', 'Sim', 'Não'])
 
-app.layout = html.Div([
-    html.H1(children='Anime', style={'textAlign':'center'}),
-    dcc.Graph(figure=hist)
-])
+# Aplicar filtros
+df_filtrado = df[(df['Init_year'].between(ano_filtro[0], ano_filtro[1])) & 
+                 (df['Genre'].isin(genero_filtro) if 'Todos' not in genero_filtro else True) & 
+                 ((df['Ongoing'] == 'Yes') if ongoing_filtro == 'Sim' else (df['Ongoing'] == 'No') if ongoing_filtro == 'Não' else True)]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Exibir tabela com os resultados
+st.write('### Tabela de Animes')
+st.dataframe(df_filtrado[['Title', 'Init_year', 'Genre', 'Ongoing']].style.set_table_styles([{
+             'selector':'td',
+             'props': [('max-width', '150px'), ('world-wrap', 'break-word')]}]), 
+             use_container_width=True)
+
+# Criar gráfico interativo usando Plotly
+fig = px.scatter(df_filtrado, x='Init_year', y='User Rating', color='Genre', hover_data=['Title'], title='Gráfico de User Rating por Ano e Gênero')
+st.plotly_chart(fig)
